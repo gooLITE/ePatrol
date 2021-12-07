691de97f39346a0e3dfec0f1c21e52337e7a8bbf
gooLITE <goolitesum@gmail.com>
2021-12-08 01:44:32 +0800

    initial commit

---
+++ ePatrol/View/RouteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RouteTableViewCell.swift
+//  ePatrol
+//
+//  Created by Yu Sum Yung on 8/12/2021.
+//
+
+import UIKit
+
+class RouteTableViewCell: UITableViewCell {
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
